a66afc967ff257915145a8d5061ae150ff261e65
Ehsan <97617842+Ehsanalizadeh78@users.noreply.github.com>
2025-08-13 23:00:40 +0330

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,18 @@
 // src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qzrmrecfzbzhfbkeckau.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6cm1yZWNmemJ6aGZia2Vja2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5NTMzMjYsImV4cCI6MjA2NzUyOTMyNn0.9pI9enDxhf7cVD9mpbfANjX5cDsGdJSiyQicKTdSQg0";
+const supabaseUrl = "https://focaqgkpphkuezccvvaz.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvY2FxZ2twcGhrdWV6Y2N2dmF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMjk2NDIsImV4cCI6MjA2NzkwNTY0Mn0.5cHOZTI96P7iqCwY60mIu5cERsNnBAZwyxflYHLN5EA";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+const { data: userData } = await supabase.auth.getUser();
+console.log("User ID:", userData.user.id);
+
+const { data, error } = await supabase.storage
+  .from("uploads")
+
+if (error) {
+  console.error("Upload error:", error.message);
+} else {
+  console.log("Upload success:", data);
+}
